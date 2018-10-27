95e03bd700d8267850d1c151e7e946bd1fc0df87
student <student@lab-44-9.cs.put.poznan.pl>
2018-10-27 10:16:07 +0200

    Extract bank interface

---
+++ MiASI_Bank/IBank.cs
@@ -0,0 +1,16 @@
+﻿namespace MiASI_Bank
+{
+    public interface IBank
+    {
+        System.Boolean DodajKredyt(RachunekBankowy rachunek);
+        System.Boolean DodajLokate(Wlasciciel wlasciciel, Kwota kwota);
+        System.Boolean DodajRachunek(Wlasciciel wlasciciel);
+        System.Boolean GenerujRaport();
+        Raport PobierzSaldo(ProduktBankowy produkt);
+        System.Boolean WplacGotowke(ProduktBankowy cel, Kwota kwota);
+        System.Boolean WykonajPrzelew(ProduktBankowy zrodlo, ProduktBankowy cel, Kwota kwota);
+        System.Boolean WyplacGotowke(ProduktBankowy zrodlo, Kwota kwota);
+        System.Boolean ZamknijRachunek(Wlasciciel wlasciciel);
+        System.Boolean ZerwijLokate(NumerProduktu numerProduktu);
+    }
+}
