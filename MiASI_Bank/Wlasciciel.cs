9d29eb5e6fb304283a36735782c04b3be6a228d5
norlesny <norlesny@gmail.com>
2018-10-27 11:17:22 +0200

    Początki obsługi inputu

--- MiASI_Bank/Wlasciciel.cs
+++ MiASI_Bank/Wlasciciel.cs
@@ -2,6 +2,12 @@ namespace MiASI_Bank
 {
     public class Wlasciciel
     {
-        
+        public string Name { get; }
+        public int Id { get; }
+
+        public Wlasciciel(string name)
+        {
+            Name = name;
+        }
     }
 }
