b3b1739bb3f4c0c86e372928faa0167c0c25c0f1
norlesny <norlesny@gmail.com>
2018-12-15 11:04:40 +0100

    Blokowanie komendy

--- MiASI_Bank/InterfejsBanku/Commands/DodajRachunekCommand.cs
+++ MiASI_Bank/InterfejsBanku/Commands/DodajRachunekCommand.cs
@@ -2,10 +2,22 @@ using System;
 
 namespace MiASI_Bank.InterfejsBanku.Commands
 {
-	public class DodajRachunekCommand:BaseBankCommand
+	public class DodajRachunekCommand : BaseBankCommand
 	{
-		public DodajRachunekCommand(IBankAccessor bank):base(bank, "dodaj-rachunek"){}
+		public DodajRachunekCommand(IBankAccessor bank) : base(bank, "dodaj-rachunek")
+		{ }
 
+		public override bool CanExecute(out string reasonMessage)
+		{
+			if (bank.Konto == null)
+			{
+				reasonMessage = "Brak zalogowanego konta";
+				return false;
+			}
+
+			reasonMessage = "Konto jest zalogowane";
+			return true;
+		}
 
 		public override void Invoke(string[] param)
 		{
